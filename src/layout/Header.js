cb2cb2407180bda345cde48d0b3de36181797e7a
Hasan Ashrafi <yasashrafi@gmail.com>
2025-01-16 11:29:28 +0330

    Initial commit from Create Next App

---
+++ src/layout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='flex items-center justify-around h-64 bg-[#473FAB] p-2'>
+          <p></p>
+          <div className='flex items-center'>
+            
+          </div>
+          <button>Upload cv</button>
+
+        </div>
+    )
+}
+
+export default Header
